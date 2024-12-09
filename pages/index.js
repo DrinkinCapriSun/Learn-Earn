0ce5f5b5150335f155fbd284caf022fd1ab231d6
DrinkinCapriSun <aguirreshiwei69@gmail.com>
2024-12-09 18:52:45 +0100

    Updated frontend files and fixed pages structure

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-import Navbar from "../components/Navbar";
-
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <h1>Welcome to My Website!</h1>
-      <p>This is the start of something amazing. Let's build this together!</p>
-    </div>
+    <>
+      <nav>
+        <div>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </nav>
+      <div className="container">
+        <h1>Welcome to My Website!</h1>
+        <p>
+          This is the start of something amazing. Let's build this together!
+        </p>
+      </div>
+      <footer>
+        <p>&copy; 2024 My Website. All rights reserved.</p>
+      </footer>
+    </>
   );
 }
